0351327c315ae9011c3f05a1dee5455b85106e7f
Azizjon Nosiraliev <ZackBoom1009@gmail.com>
2025-03-13 13:35:07 -0400

    add routes and listen function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const data = require("./data");
+// get the data from the array of objects:
+const animals = require("./data");
+
+// init express function module and save it as an express var:
+const express = require("express");
+// init the express module and save it to app var:
+const app = express();
+// init the PORT"
+const PORT = 3008;
+
+// home route:
+app.get("/", (req, res) => {
+    res.send('<h1> Welcome to my Assignment page</h1>');
+});
+
+// animals route:
+app.get("/animals", (req, res) => {
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+    res.send(randomAnimal);
+});
+
+// This listen runs the PORT and starts the server:
+app.listen(PORT, () => {
+    console.log(`Server running at localhost:${PORT}`);
+});
