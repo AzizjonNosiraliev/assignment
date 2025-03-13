a10c5d4156ffa42fcab999503eaecef01ad13257
Azizjon Nosiraliev <ZackBoom1009@gmail.com>
2025-03-13 14:31:40 -0400

    add body-parser and post route

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // get the data from the array of objects:
 const animals = require("./data");
-// init express function module and save it as an express var:
+// init express function module and save it as an express var 
+// and body-parser for parsing JSON data in POST requests.:
 const express = require("express");
+const bodyParser = require('body-parser');
 // init the express module and save it to app var:
 const app = express();
 // init the PORT"
 const PORT = 3008;
+
+//use body-parser middleware to parse JSON data in POST requests:
+app.use(bodyParser.json());
+// POST route to add a new animal to the data file:
+app.post("/animals", (req, res) => {
+    const newAnimal = req.body;
+    animals.push(newAnimal);
+    res.json({"New animal added": newAnimal});
+});
 
 // home route:
 app.get("/", (req, res) => {
